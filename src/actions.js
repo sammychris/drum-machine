23959064170dec756e8132b65eb5e85c0d916a80
sammychris <ebusameric@gmail.com>
2020-04-24 13:16:25 +0100

    refactor: Add redux actions for state management

---
+++ src/actions.js
@@ -0,0 +1,14 @@
+export const setSoundName = (value) => ({
+  type: "DRUM_SOUNDNAME",
+  payload: value,
+});
+
+export const setSwitched = (value) => ({
+  type: "DRUM_SWITCHED",
+  payload: value,
+});
+
+export const setPower = (value) => ({
+  type: "DRUM_POWER",
+  payload: value,
+});
